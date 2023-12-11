cc881d76d283b97cec99f0b681db165b0d76e3e5
Sameer Faridi <s.faridi007@gmail.com>
2023-12-11 21:19:59 +0530

    remve all files

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const path = require("path");
-const { home, login, signup } = require("./controller/user.controller");
+const bcryptjs = require("bcryptjs");
 
 dotenv.config();
 
-const port = process.env.PORT || 6969;
+const port = 6969;
 const app = express();
 
 // Middleware
@@ -16,14 +17,68 @@ app.use(cors());
 
 // MongoDB connection
 mongoose
-  .connect(process.env.MONGOURI)
+  .connect("mongodb+srv://admin:admin123@cluster0.w1vziuo.mongodb.net/")
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    }
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
 // Routes
-app.get("/", home);
-app.post("/register", signup);
-app.post("/login", login);
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+app.post("/register", async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword
+  });
+
+  try {
+    await newUser.save();
+    res.status(201).json({ message: "User created successfully!" });
+  } catch (error) {
+    console.log("Error in signup:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+app.post("/login", async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return res.status(404).json({ error: "User not found!" });
+    }
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return res.status(401).json({ error: "Wrong credentials!" });
+    }
+
+    res.status(201).json({ message: "Login successful!" });
+  } catch (error) {
+    console.log("Error in login:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 // Error handling middleware
 app.use((err, req, res, next) => {
